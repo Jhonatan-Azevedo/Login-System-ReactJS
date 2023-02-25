c6ff52e49b00625e982d15f1bc0feb086db84c28
Jhonatan Azevedo <dev.azevedo@outlook.com>
2023-02-25 16:07:59 -0300

    ADD: footer login

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -8,6 +8,7 @@ export const Login = () => {
     const [password, setPassword] = useState<string>("");
     const [showAlert, setShowAlert] = useState<boolean>(false);
     const [loading, setloading] = useState<boolean>(false);
+    const [seePassword, setSeePassword] = useState<boolean>(false);
 
     const auth = useAuth();
     const navigate = useNavigate();
@@ -21,6 +22,14 @@ export const Login = () => {
         return myAlert.classList.remove("fade");
     }, [showAlert])
 
+    useEffect(() => {
+        const inputPass = document.querySelector("#password") as HTMLInputElement;
+        if (seePassword) {
+            inputPass.type = "text"
+            return;
+        }
+        inputPass.type="password"
+    }, [seePassword])
 
     function timeout(ms: number) {
         return new Promise(resolve => setTimeout(resolve, ms));
@@ -49,12 +58,18 @@ export const Login = () => {
             <div className="w-50 h-100 field-bg-login">
 
             </div>
-            <div className="p-5 field-form-login">
+            <div className="p-5 field-form-login position-relative d-flex justify-content-center align-items-center">
                 <form className="w-75 mx-auto">
                     <h4 className="mb-4">Access</h4>
-                    <input className="border-0 border-bottom w-100 mb-3 p-2 outline-0" type='text' placeholder="Email" value={email} onChange={(event) => setEmail(event.target.value) } />
-                    <input className="border-0 border-bottom w-100 mb-3 p-2 outline-0" type='password' placeholder="Password" value={password} onChange={(event) => setPassword(event.target.value) }/>
+                    <input className="input-custom border-0 border-bottom w-100 mb-3 p-2 outline-0" type='text' placeholder="Email" value={email} onChange={(event) => setEmail(event.target.value) } />
                     
+                    <div id="divInput" className="border-bottom p-0 m-0 d-flex align-items-center">
+                        <input id="password" className="input-custom border-0 p-2 outline-0 w-100" type='password' placeholder="Password" value={password} onChange={(event) => setPassword(event.target.value)} />
+                        { !seePassword ?
+                            <i className="input-custom bi bi-eye text-secondary p-1" onClick={() => setSeePassword(true)}></i> : 
+                            <i className="bi bi-eye-slash text-secondary p-1" onClick={() => setSeePassword(false)}></i>
+                        }
+                    </div>
                     <div className="small w-100 text-start mb-3">
                         <a href="#" className="link-secondary text-decoration-none link-custom ">Forgot password?</a>
                     </div>
@@ -81,7 +96,19 @@ export const Login = () => {
                     </div>
                 </form>
 
-                
+                <footer className="footer-login text-center text-secondary">
+                    <span>
+                        <div>
+                            <a href="https://www.linkedin.com/in/jazevedodev/" target="_blank" title="Linkedin @Jhonatan Azevedo" className="link-secondary link-custom">
+                                <i className="bi bi-linkedin mx-2 fs-2"></i>
+                            </a>
+                            <a href="https://github.com/Jhonatan-Azevedo"  target="_blank" title="Github @Jhonatan-Azevedo" className="link-secondary link-custom">
+                                <i className="bi bi-github fs-2"></i>
+                            </a>
+                        </div>
+                        Desenvolvido por Jhonatan Azevedo
+                    </span>
+                </footer>
             </div>
         </section>
     )
